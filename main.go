ed9fdbe8f3f972438915aaef2af5c204d232b3fd
Yuki Matsushita <ymatsushita@picniic.com>
2017-12-25 10:43:20 -0800

    add tmp foloder to gitignore

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "1111",
+			"message": "1121",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
